3160757973dadfe6eddc8de60e629ca77f59e540
Marques Ivory <Maivory12@icloud.com>
2023-04-06 17:23:04 -0400

    condition to require typedefs and resolvers in server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 const {ApolloServer} = require ('apollo-server-express');
-
+const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
 // Set up the Apollo Server
